a396aeb89b02df2daecdca9dfa5893ba433e612e
Emiliano <vic_emiliano@hotmail.com>
2023-08-17 15:07:48 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       Bienvenido Emiliano
+       Bienvenido Emiliano te ayudo soy Ger
         </p>
         <a
           className="App-link"
