e127523b1223337bd00bcf8003d65586d3bcbb47
Emiliano <vic_emiliano@hotmail.com>
2023-08-22 15:27:07 -0300

    cambio de imagen de perfil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import emiliano from "./assets/img/WhatsApp Image 2023-08-22 at 14.45.17.jpeg";
+import logo from './emiliano.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       Bienvenido Emiliano 
+        <img src={emiliano} className="App-logo" alt="logo" />
+        <p>Bienvenido Emiliano 
         </p>
         <a
           className="App-link"
